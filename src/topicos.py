367b3e1744f50b2ecf527a6076c1f01531561350
bernarducs <bernarducs@gmail.com>
2024-05-27 18:32:51 -0300

    separação dos tópicos

---
+++ src/topicos.py
@@ -0,0 +1,59 @@
+
+lista_de_topicos = [
+    {
+        "id": "1",
+        "image": "https://i.ibb.co/7jfcX2m/questao-menor.png",
+        "label": "Intro",
+        "description": "O que nos motivou a estar aqui",
+        "content": "Bender Bending Rodríguez, (born September 4, 2996), designated Bending Unit 22, and commonly "
+        "known as Bender, is a bending unit created by a division of MomCorp in Tijuana, Mexico, "
+        "and his serial number is 2716057. His mugshot id number is 01473. He is Fry's best friend.",
+    },
+    {
+        "id": "2",
+        "image": "https://i.ibb.co/pZQm7J6/profiling-menor.png",
+        "label": "Em busca de um perfil",
+        "description": "Será que existe?",
+        "content": "Carol Miller (born January 30, 2880), better known as Mom, is the evil chief executive officer "
+        "and shareholder of 99.7% of Momcorp, one of the largest industrial conglomerates in the universe "
+        "and the source of most of Earth's robots. She is also one of the main antagonists of the Futurama "
+        "series.",
+    },
+    {
+        "id": "3",
+        "image": "https://img.icons8.com/clouds/256/000000/homer-simpson.png",
+        "label": "Mas onde é que entra linguagens de programação?",
+        "description": "Overweight, lazy, and often ignorant",
+        "content": "Homer Jay Simpson (born May 12) is the main protagonist and one of the five main characters of "
+        "The Simpsons series(or show). He is the spouse of Marge Simpson and father of Bart, "
+        "Lisa and Maggie Simpson.",
+    },
+    {
+        "id": "homer",
+        "image": "https://img.icons8.com/clouds/256/000000/homer-simpson.png",
+        "label": "Por que Python?",
+        "description": "Overweight, lazy, and often ignorant",
+        "content": "Homer Jay Simpson (born May 12) is the main protagonist and one of the five main characters of "
+        "The Simpsons series(or show). He is the spouse of Marge Simpson and father of Bart, "
+        "Lisa and Maggie Simpson.",
+    },
+    {
+        "id": "homer",
+        "image": "https://img.icons8.com/clouds/256/000000/homer-simpson.png",
+        "label": "Primeiros passos",
+        "description": "Overweight, lazy, and often ignorant",
+        "content": "Homer Jay Simpson (born May 12) is the main protagonist and one of the five main characters of "
+        "The Simpsons series(or show). He is the spouse of Marge Simpson and father of Bart, "
+        "Lisa and Maggie Simpson.",
+    },
+    {
+        "id": "homer",
+        "image": "https://img.icons8.com/clouds/256/000000/homer-simpson.png",
+        "label": "Mas onde é que entra linguagens de programação?",
+        "description": "Overweight, lazy, and often ignorant",
+        "content": "Homer Jay Simpson (born May 12) is the main protagonist and one of the five main characters of "
+        "The Simpsons series(or show). He is the spouse of Marge Simpson and father of Bart, "
+        "Lisa and Maggie Simpson.",
+    },
+]
+
