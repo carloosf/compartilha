8b04e071a594c1160002a9fc2240d9038cf606e3
bernarducs <bernarducs@gmail.com>
2024-06-06 01:38:26 -0300

    processo finalizado

--- src/contents/processo.py
+++ src/contents/processo.py
@@ -2,26 +2,59 @@ from dash import dcc
 
 content = dcc.Markdown('''
 
-- Você vai errar muito e isso é normal, estou nisso faz tempo e continuo 
-errando muito. É uma aba do Colab aberto e outras 10 pedindo ajuda a internet. A sacada é saber a quem recorrer quando estiver empacado. Como eu já venho errando muito já tenho uma lista com recursos de ajuda. Errar criará a sua;                                              
+#### Dica 1: Faça como as crianças fazem, elas fazem!
+- Decida logo um meio de aprendizado (uma playlist no Youtube, ou um livro, ou algum tutorial 
+na internet) e comece a codar no Colab. Só vai. Não faça um mega planejamento, nem pense 
+que no final de semana irá começar, porque você não vai. Mente de adulto é cheia de requisitos. 
+Crianças se jogam em qualquer coisa porque elas não sabem nada da vida e ninguém ainda não as 
+                       encheu suficientemente de porquês e tão loucas para experimentar.
 
-- Projetos são legais porque eles têm um fim claro, ilustram casos do 
-cotidiano e abordam diversas áreas da programação. 
-Um simples baixador de vídeo pode envolver o básico de python como 
-condicionais, listas, loops; imports de bibloteca; 
-entender como funciona a api dessa biblioteca; tarefas de input e output (IO), etc;
+![](https://humordido.net/wp-content/uploads/2017/04/asiaticos.gif)                       
                        
-- Tenha paciência, siga um projeto de aprendizado e siga ele com persistência. 
-O divertido é achar massa os pequenos avanços que são dados. 
-Procure pelas comunidades, sempre haverá pessoas disposta ajudar. 
-Fora que, agora com os GPTs, facilitou muito encontrar pelo menos um caminho;
+#### Dica 2: Aprenda com projetos simples.
+- Projetos são legais porque eles têm um fim claro, ilustram casos 
+reais e abordam diversas áreas da programação. 
+Um simples baixador de vídeo, uma mapa com as escolas estudais plotadas, 
+uma email com a posição de empenhado e liquidado que se atualiza todos os dias 
+envolvem o básico de python como condicionais, listas, loops; imports de pacotes; 
+Traduzindo: você realiza coisas enquanto vai lidando com a teoria. 
+                       Ter retornos relativamente rápidos te motivam a prosseguir;
+
+![](https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmhyOXU1dmo1MjNyZDdzOHZiZGx0ZDQybzkxenFjcjFwcDZteTZwOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/f7b9ltJ4FrhnsKjYx2/giphy.webp)                        
+
+#### Dica 3: Não vai ficar bom mas e daí?                                                                     
+- Você vai errar muito e isso é normal, estou nisso faz tempo e continuo 
+errando muito. É uma aba do Colab aberto e outras 10 procurando soluções. 
+Claro, meus erros de hoje não são os de 2017 quando comecei.
+Como eu já venho errando muito já tenho uma lista com recursos de ajuda. Errar criará a sua.
+E o principal: não se cobre muito, muito menos se compare com outras pessoas ("nunca vou ser fulando de tal") 
+                       e sim, meça seus avanços em relação a você mesmo;   
+
+![](https://i.makeagif.com/media/8-11-2016/P4LcQP.gif)
+                      
+#### Dica 4: Code todos os dias.                        
+- Code todo dia uma etapa do seu projeto e tá ótimo. Contanto que você continue amanhã. E depois de amanhã. 
+É pra criar memória muscular como se fosse aprender inglês pra valer - manter a 
+conversação com certa frequência por um longo período.
+O divertido é achar massa os pequenos avanços que são dados. Um amigo que é bom desenhista 
+me falou que ele só se tornou bom porque ele foi o único da turma que quis continuar a desenhar.
+O tempo vai te fazer perceber que as perguntas que você faz ao 
+GPT são mais balizadas recebendo respostas melhores da IA. É bom sinal que você tá aprendendo.
+
+![](https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTUwanMyNzBrcmltcms0YWx0dW81MzR5cjc3NGR1cTB6cWQ2ZmpjNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/V1wiixQS9Nuw0/giphy.webp)                                              
+
+#### Dica 5: Compartilhe suas ~~dores~~ dúvidas. 
+- Persistência deve vir acompanhada com a consistência. Ficar errando no mesmo tópico 
+também é desestimulante, convenhamos. Então saiba como pesquisar e pedir ajuda. 
+Converse com alguém que você acha que está mais a frente que você ou procure comunidades. 
+Não pergunte de todo jeito, ofereça um contexto do que você tá pretendendo fazer, as soluções 
+fracassadas, o que você acha que pode ser mas não consegue implementar e claro, mostre o código.
+Isso encurtará caminhos (= sobe lá no 3o andar). 
+
+![](https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTYwNHBta3dmZDFtdmt2Z2lraXRkbGlvZXV6cW9yNHVwcnZkb2gzeSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/hSvan0ooZNv6NlXOqV/giphy.webp)                       
                        
-- Persistência deve vir acompanhada com a consistência. Faça um curso, 
-procure um livro de referência e principalmente, 
-converse com alguém que você acha que está mais a frente que você. 
-Isso encurtará caminhos. Vai perceber que suas perguntas que você fará ao 
-GPT te dará respostas melhores dele.
-                       
-- No fim, se todos aqueles pontos citados por Brooks aconteceu com você, 
-                       então acredito que já teremos uma novo ou nova programadora!
+### No fim, se todos aqueles pontos citados por Brooks aconteceu com você, 
+então acredito que já teremos uma novo ou nova programadora!
+
+![](https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWhoa2g0Y2FiMTRnbWdpNWFzY2N5N2Q1aDExbXc3NWtnMzA2NGMydiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/11kcEW8uNh8RwY/giphy.webp)                       
 ''')
