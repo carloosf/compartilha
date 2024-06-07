663be3c880669d58e7004551de01e29a549eeaf8
bernarducs <bernarducs@gmail.com>
2024-06-07 15:17:36 -0300

    dicas finalizado

--- src/contents/dicas.py
+++ src/contents/dicas.py
@@ -2,18 +2,64 @@ from dash import dcc
 
 content = dcc.Markdown('''
 
-- Curso Intensivo de Python;
+### Pra quem gosta de vídeo                       
 
-- Live de Python, que conta com vídeo de introdução a programação;
+No youtube tem centenas de cursos e tutoriais. E é claro, vai ter muita picaretagem. 
+É muita gente querendo engajar mais que ensinar, fazer curso pra vender outro curso, ou simplesmente, didática.                       
+Um que eu indicaria é esse:
+
+[Hashtag treinamentos](https://www.youtube.com/playlist?list=PLpdAy0tYrnKwgyv8Rc867jA_huQfcpF29). 
                        
-- Pense Python;
+- É formado por planilheiros que viraram programadores (lembra alguém?);
+- O curso é desse ano;                       
+- Apenas 9 vídeos cuja maioria tem de 30 a 20 minutos;
+- O projeto não aparenta ser tão empolgante, mas acredite é muito útil no dia a dia;
+- Você pode gastar os 10 minutinhos iniciais nos preparativos ou fazer tudo 
+no Colab (só lembrar de ativar o google drive para salvar arquivos)    
+
+Outro que gosto muito:
                        
-- Persistência deve vir acompanhada com a consistência. Faça um curso, 
-procure um livro de referência e principalmente, 
-converse com alguém que você acha que está mais a frente que você. 
-Isso encurtará caminhos. Vai perceber que suas perguntas que você fará ao 
-GPT te dará respostas melhores dele.
+[Live de Python](https://www.youtube.com/@Dunossauro)
+- Ele programa para se [divertir](https://www.youtube.com/live/t5uxBACjp8Y?si=RZOWTIOaYqOLnIuR&t=6251);
+- Em matéria de conteúdo vai além do que outros canais ficam promovendo;
+- Tem uma comunidade que ajuda muito no [telegram](https://web.telegram.org/a/#-1001142824279);
+- Tem vídeo sobre [COMO](https://www.youtube.com/watch?v=HSotf1Amess) aprender python;
+- Nesse mesmo vídeo ele mostra um [roadmap](https://www.youtube.com/watch?v=HSotf1Amess&t=148s) de aprendizado;
+- E claro, [um vídeo](https://www.youtube.com/watch?v=yTQDbqmv8Ho&list=PLOQgLBuj2-3LwyKrvIde88moVcFWAN2Xz&index=2) de 4 horinhas de introdução à linguagem.                                                                                                                                                               
+
+### Pra quem gosta de livros
                        
-- No fim, se todos aqueles pontos citados por Brooks aconteceu com você, 
-                       então acredito que já teremos uma novo ou nova programadora!
+[Curso Intensivo de Python](https://tinyurl.com/3j69mdzb). Eu comecei na linguagem por ele e é a minha 
+maior recomendação. A mescla entre teoria e projetos é fantástica.
+Você começa fazendo um pequeno jogo, depois entra na área de dados e 
+termina com um aplicação web. Contras: Ele é meio carinho.
+
+[Pense Python](https://penseallen.github.io/PensePython2e/). Ótimo conteúdo e tá todo na internet liberado pelo próprio autor. Não é focado em projetos.
+                       
+### Comunidades
+
+Lembre-se daqueles fatores que fazem as pessoas curtirem programar. 
+Uma delas é compartilhar, tem outra que diz sobre fazer coisas para outroas pessoas... 
+O sucesso de se transformar em uma pessoa que coda é estar imerso a outras pessoas que fazem a mesma coisa.
+Aí é que entram as comunidades. Em recife temos vários grupos onde pessoas se ajudam, jogam conversa foram e até se reunem!
+
+![image](https://i.ibb.co/p3zjM8J/9383c783-b3c8-484a-adc2-85a74ff4578e.jpg)
+                       
+[Grupo de usuários de Python - Pernambuco](https://web.telegram.org/a/#-1001098850103)                       
+Grupo serve como um hub de informações das maiorias de eventos que estejam acontecendo no estado.                       
+
+[DOJO Recife](https://web.telegram.org/a/#-1001935352231)                       
+Um coding dojo é uma técnica de aprendizagem colaborativa onde pessoas se encontram em um
+                       espaço que permitam utilizar notebook e projetar conteúdo e tentam 
+resolver um problema definido previamente. 
+O intuito é ter pessoas compartilhando com outras quanto as metodologias e técnicas da linguagem.
+                       
+[PyLadies](https://web.telegram.org/a/#-1001455493854)       
+Como a própria descrição da comunidade diz "Comunidade com objetivo de ajudar 
+mais mulheres a se tornarem participantes líderes ativas na comunidade de código aberto Python".  
+É uma iniciativa que começou em Los Angeles e foi trazida ao Brasil em 2014 e hoje conta com 30 grupos ativos.
+
+### Quando todo mundo se reune
+![image](https://i.ibb.co/mb1cCw7/python-ne.png)
+          
 ''')
