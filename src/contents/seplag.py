c80d14dc43e2c745b59df51da34698caf0af4112
bernarducs <bernarducs@gmail.com>
2024-06-10 01:24:08 -0300

    revisando..

--- src/contents/seplag.py
+++ src/contents/seplag.py
@@ -3,16 +3,13 @@ from dash import dcc
 
 content = dcc.Markdown('''
 
-### Nìvel Padawan - automatize ou pelo menos reduza o tempo de rotinas
-
 - Crie e agregue planilhas em um bloco de código;
                        
 - Baixe, mova ou faça uploads de arquivos de forma agendada;
 
-- Mande informações por e-mail.
-                                                                     
+- Mande informações por e-mail;              
 
-### Nìvel Jedi - comece a brincar com ciência de dados
+- Geolocalize endereços;
 
 - Utiize séries temporais para observar tendências orçamentárias, faturamento 
 hospitalar, capacidade do sistema prisional;
@@ -20,8 +17,6 @@ hospitalar, capacidade do sistema prisional;
 - Crie web-apps, onde usuários cadastram dados de forma fácil e com validação 
 de dados;
                        
-- Crie um hub de dashboards;
-
-- 
+- Crie um hub de dashboards.
                        
 ''')
